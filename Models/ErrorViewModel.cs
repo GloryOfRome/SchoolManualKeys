ac86831675318040853d1e04e0616f8f92465e18
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-15 20:56:59 -0500

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolManualKeys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
